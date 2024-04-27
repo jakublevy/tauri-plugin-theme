28cd2b411928af0f64ea0565aa647f13260f4258
Jakub Levý <jakub.levy15@gmail.com>
2024-04-27 16:02:10 +0200

    do not restart application on windows

--- tauri-plugin-theme/src/platform/windows.rs
+++ tauri-plugin-theme/src/platform/windows.rs
@@ -4,6 +4,5 @@ use tauri::{command, AppHandle, Runtime};
 #[command]
 pub fn set_theme<R: Runtime>(app: AppHandle<R>, theme: Theme) -> Result<(), &'static str> {
     save_theme_value(&app, theme);
-    app.restart();
     Ok(())
 }
